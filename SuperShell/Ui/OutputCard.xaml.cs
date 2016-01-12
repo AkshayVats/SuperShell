37f4799519967cc16f9f37dfd89a73afea5590bf
Akshay <akshayvats@outlook.com>
2016-01-12 15:14:19 +0530

    shell card

---
+++ SuperShell/Ui/OutputCard.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SuperShell.Ui
+{
+    /// <summary>
+    /// Interaction logic for OutputCard.xaml
+    /// </summary>
+    public partial class OutputCard : UserControl
+    {
+        public OutputCard()
+        {
+            InitializeComponent();
+        }
+
+        internal enum OutputType { Error, Normal, Gray }
+
+        internal void Render(object obj, OutputType typ)
+        {
+            var output = Output.OutputManager.GenerateOutput(obj);
+            if (!(output is Output.Viewers.OutputWithType))
+                typ = OutputType.Gray;
+            Content = output;
+            Style = GetStyle(typ);
+            if ((output as Control) != null)
+                (output as Control).Foreground = Foreground;
+        }
+        internal void RenderMessage(string msg, OutputType typ)
+        {
+            var output = Output.OutputManager.GenerateMessage(msg);
+            Content = output;
+            Style = GetStyle(typ);
+            if ((output as Control) != null)
+                (output as Control).Foreground = Foreground;
+        }
+        internal void RenderProperty(string propertyName, object value, OutputType typ)
+        {
+            var output = new TextBlock()
+            {
+                Padding = new Thickness(2),
+                Inlines = {
+                    new Bold(new Run(propertyName) { Foreground = new SolidColorBrush(Colors.Red) }),
+                    new Run(" : " + value.ToString()) { Foreground = new SolidColorBrush(Colors.Black) }
+                    }
+            };
+            Content = output;
+            Style = GetStyle(typ);
+            
+        }
+
+        private Style GetStyle(OutputType typ)
+        {
+            switch (typ)
+            {
+                case OutputType.Error: return FindResource("ErrorStyle") as Style;
+                case OutputType.Normal: return FindResource("NormalOutputStyle") as Style;
+                case OutputType.Gray:return FindResource("GrayOutputStyle") as Style;
+            }
+            return null;
+        }
+    }
+}
---
+++ SuperShell/Ui/ShellCard.xaml.cs
@@ -0,0 +1,103 @@
+﻿using SuperShell.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace SuperShell.Ui
+{
+    /// <summary>
+    /// Interaction logic for InputControl.xaml
+    /// </summary>
+    public partial class ShellCard : UserControl
+    {
+        private ICardManager _cardManager;
+        public bool HasOutput { get; private set; }
+        public bool IsEvaluated { get; private set; }
+        private Bridge.Ui.IShellInputControl input;
+        internal ShellCard(ICardManager cardManager):base()
+        {
+            InitializeComponent();
+            AddEditor();
+            _cardManager = cardManager;
+        }
+
+        private void AddEditor()
+        {
+            input = Plug.PluginManager.CodeEditorProvider.GenerateShellInput();
+            var editorUi = input.Control;
+            editorUi.Padding = new Thickness(5);
+            input.CommandEntered += ShellInputControl_CommandEntered;
+            grid.Children.Add(editorUi);
+        }
+
+        private void ShellInputControl_CommandEntered(object sender, string e)
+        {
+            EvaluateCommand(e);
+        }
+
+        private void EvaluateCommand(string e)
+        {
+            input.Evaluating = true;
+            IsEvaluated = false;
+            input.Control.Style = FindResource("shell_evaluating") as Style;
+            ClearCardOutput();
+            var result = Evaluator.Inst.Evaluate(e);
+            if (result.Errors?.Count() > 0)
+            {
+                var output = new OutputCard();
+                output.RenderMessage(String.Join("\n", result.Errors), OutputCard.OutputType.Error);
+                stackPanel.Children.Add(output);
+                input.Evaluating = null;
+                input.Control.Style = FindResource("shell_active") as Style;
+            }
+            else
+            {
+                if (result.Result != null)
+                {
+                    var output = new OutputCard();
+                    output.Render(result.Result, OutputCard.OutputType.Normal);
+                    stackPanel.Children.Add(output);
+                }
+
+                IsEvaluated = true;
+                input.Control.Style = FindResource("shell_inactive") as Style;
+                HasOutput = true;
+                input.Evaluating = false;
+                _cardManager.AddEmptyCard();
+            }
+        }
+
+        private void ClearCardOutput()
+        {
+            HasOutput = true;
+            while (stackPanel.Children.Count > 1)
+                stackPanel.Children.RemoveAt(1);
+        }
+        public new void Focus()
+        {
+            Dispatcher.BeginInvoke(DispatcherPriority.Input,
+                new Action(delegate () {
+                    input.Control.Focus();         // Set Logical Focus
+                    Keyboard.Focus(input.Control); // Set Keyboard Focus
+                }));
+        }
+        public void AppendInput(string str)
+        {
+            int z = input.CaretOffset + str.Length;
+            input.Text = input.Text.Insert(input.CaretOffset, str);
+            input.CaretOffset = z;
+        }
+    }
+}
